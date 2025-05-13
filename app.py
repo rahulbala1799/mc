b4707259ee1d75ff06cc791da13ad82e1988fda7
Rahul <rahul@printnpack.ie>
2025-05-13 16:48:46 +0100

    Change default port to 5001 and force redeploy

--- app.py
+++ app.py
@@ -2120,5 +2120,5 @@ def backlog_analysis():
         return redirect(url_for('ticket_overview'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=False) 
